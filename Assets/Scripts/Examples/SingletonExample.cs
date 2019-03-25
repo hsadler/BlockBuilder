6c81d5880a1a750e4527b58754de4b22054638ca
Harry Sadler <harrysadlermusic@gmail.com>
2019-03-24 21:48:24 -0700

    stubbing out more setup

---
+++ Assets/Scripts/Examples/SingletonExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonExample : MonoBehaviour
+{
+    
+    // the static reference to the singleton instance
+    public static SingletonExample instance { get; private set; }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        // singleton pattern
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/SceneManagement/EnvironmentGeneration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentGeneration : MonoBehaviour {
+
+	// RESPONSIBLE FOR PROCEDURAL ENVIRONMENT GENERATION
+
+	// the static reference to the singleton instance
+    public static EnvironmentGeneration instance { get; private set; }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        // singleton pattern
+        if(instance == null) {
+            instance = this;
+            // DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
