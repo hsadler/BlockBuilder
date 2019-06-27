05b1a6c561ba0286359d3f6c90c5b07ff5c2df71
Harry Sadler <harrysadlermusic@gmail.com>
2019-06-26 22:08:02 -0700

    fix block orientation control to be more intuitive

--- Assets/Scripts/SceneObjects/Player/PlayerInventoryScript.cs
+++ Assets/Scripts/SceneObjects/Player/PlayerInventoryScript.cs
@@ -89,11 +89,13 @@ public class PlayerInventoryScript : MonoBehaviour
 	}
 
 	public void RotateGhostBlock(Vector3 direction) {
-		// TODO: rotates based on initial orientation, so not intuitive
-		// rotation should be done where origin is current orientation
 		if(ghostBlockGO.activeSelf) {
 			Quaternion addRotation = Quaternion.Euler(direction * 90.0f);
-			Quaternion newRotation = ghostBlockGO.transform.rotation * addRotation;
+			// CODE EXAMPLE: Putting addRotation before the GhostBlock rotation causes the 
+			// resulting rotation to be from a world perspective instead of local. I have
+			// no idea why this works
+			// RESOURCE: https://answers.unity.com/questions/512667/smooth-rotation-about-global-axis-instead-of-local.html
+			Quaternion newRotation = addRotation * ghostBlockGO.transform.rotation;
 			ghostBlockGO.GetComponent<BaseBlockScript>().RotateBlock(
 				ghostBlockGO.transform.rotation, 
 				newRotation, 
